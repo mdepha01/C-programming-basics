2e0f69ed85a6f1e26c15f5ff5fcc12b62b0e2f79
mdepha01 <mdepha01@gmail.com>
2022-09-08 06:55:34 -0700

    task 6 commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,14 +8,14 @@ int main(void)
 {
 	char c;
 	int i;
-	long int li;
-	long long ll;
+	long int k;
+	long long l;
 	float f;
 
 	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(c));
 	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(i));
-	printf("Size of a long int: %lu bytes(s)\n", (unsigned long)sizeof(li));
-	printf("Size of a long long int: %lu bytes(s)\n", (unsigned long)sizeof(ll));
+	printf("Size of a long int: %lu bytes(s)\n", (unsigned long)sizeof(k));
+	printf("Size of a long long int: %lu bytes(s)\n", (unsigned long)sizeof(l));
 	printf("Size of a float: %lu bytes(s)\n", (unsigned long)sizeof(f));
 	return (0);
 }
