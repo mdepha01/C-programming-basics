ba54195b1c1aba27d902a27dbda552f375796457
mdepha01 <mdepha01@gmail.com>
2022-09-09 08:33:29 -0700

    task 2 commit

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+/**
+ * main - Entry point
+ * printing letters of the alphabet using putchar
+ * Return:0 Always (Success)
+*/
+
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c < 'z'; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
