5dc8a55156252171ccc8cf29deca36ff47196e31
mdepha01 <mdepha01@gmail.com>
2022-09-13 04:59:44 -0700

    task 1 commit

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *main - entry point
+ * print_alphabet- Entry point
+ * Return: void or nothing
+ */
+
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
