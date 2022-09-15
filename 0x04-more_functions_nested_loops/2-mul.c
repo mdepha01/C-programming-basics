6813c1be89ba94c7215996375442ad3be098e553
mdepha01 <mdepha01@gmail.com>
2022-09-15 09:34:25 -0700

    task 2 commit

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - finds the product of 2 intergers
+ * @a: 1st parameter passed.
+ * @b: 2nd parameter passed.
+ * Return: return product
+ */
+
+int mul(int a, int b)
+{
+	int product = 0;
+
+	product = a * b;
+	return (product);
+}
