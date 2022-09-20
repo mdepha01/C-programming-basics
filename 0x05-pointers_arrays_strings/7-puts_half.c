494927f57f2bf39b41ea3fce854808700ac05752
mdepha01 <mdepha01@gmail.com>
2022-09-19 18:55:51 -0700

    task 7 commit

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * puts_half - function that prints half of a string.
+ * @str: pointer passed as argument
+ * Return: void / nothing
+ */
+void puts_half(char *str)
+{
+	int c, a;
+	char l;
+
+	for (c = 0; str[c] != 0; c++)
+	{
+	}
+	if (c % 2 == 0)
+	{
+		for ((a = c / 2); str[a] != 0; a++)
+		{
+			l = str[a];
+			_putchar(l);
+		}
+	}
+	else
+	{
+		for ((a = (c - 1) / 2); str[a] != 0; a++)
+		{
+			l = str[a];
+			_putchar(l);
+		}
+	}
+	_putchar('\n');
+}
