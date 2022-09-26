ee09d286406bd5c7066c780d38c6d80ba6417466
mdepha01 <mdepha01@gmail.com>
2022-09-26 14:36:11 -0700

    task 7 commit

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_diagsums - prints the sum of diagonal numbers
+ * @a: pointer to array
+ * @size: size of the matrix
+ * Return: void / nothing
+ */
+
+void print_diagsums(int *a, int size)
+{
+	int i, sum1 = 0, sum2 = 0;
+
+	for (i = 0; i < (size * size); i++)
+	{
+		if (i % (size + 1) == 0)
+		{
+			sum1 += *(a + i);
+		}
+		if (i % (size - 1) == 0 && i != 0 && i < size * size - 1)
+		{
+			sum2 += *(a + i);
+		}
+	}
+	printf("%d, %d\n", sum1, sum2);
+}
