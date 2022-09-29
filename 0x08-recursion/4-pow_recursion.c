f81abe1645b1ed12b22404ff6b7787fa697dd686
mdepha01 <mdepha01@gmail.com>
2022-09-28 18:31:43 -0700

    task 4 commit

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+
+/**
+ * _pow_recursion - returns power of a number
+ * @x: base number
+ * @y: exponent
+ * Return: 1 if y =0 or -1 if y < 0
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y == 0)
+	{
+		return (1);
+	}
+	else if (y < 0)
+	{
+		return (-1);
+	}
+	else
+	{
+		return (x * _pow_recursion(x, y - 1));
+	}
+}
