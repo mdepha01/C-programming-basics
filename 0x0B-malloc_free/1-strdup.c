de94a07ae5ed22d890948781e2ef2fd836c97694
mdepha01 <mdepha01@gmail.com>
2022-10-04 16:38:09 -0700

    task 1 commit

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _strdup - copies string
+ * @str: argument for array
+ * Return: Always 0.
+ */
+
+char *_strdup(char *str)
+{
+	char *array;
+	int i = 0, len = 0;
+
+	array = malloc(sizeof(str));
+	while (str[i] != '\0')
+	{
+		len++;
+		i++;
+	}
+	if (array == NULL)
+	{
+		return (NULL);
+	}
+	i = 0;
+	while (str[i] != '\0')
+	{
+		array[i] = str[i];
+		i++;
+	}
+	return (array)
+}
