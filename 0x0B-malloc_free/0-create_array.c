5f3ebf5e2222bb7ad5ead60a96fbdcc245a09c55
mdepha01 <mdepha01@gmail.com>
2022-10-04 16:24:38 -0700

    task 0 commit

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+
+/**
+ * create_array - creates an array of size n
+ * @c: char passed as argument
+ * @size: size of array to be created
+ * Return: Null if empty or pointer to
+ * new array
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *p;
+	unsigned int i = 0;
+
+	p = malloc(sizeof(char) * size);
+	if (size == 0)
+	{
+		p = NULL;
+	}
+	else
+	{
+		for (i = 0; i < size ; i++)
+		{
+			p[i] = c;
+		}
+	}
+	return (p);
+}
