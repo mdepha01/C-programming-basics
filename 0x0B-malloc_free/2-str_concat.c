37c5823d6c415309ef52dfaee20c8bddfcdd81f8
mdepha01 <mdepha01@gmail.com>
2022-10-04 17:19:36 -0700

    task 2 commit

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * str_concat - function that concatenates two strings
+ * @s1: string 1
+ * @s2: string 2
+ * Return: Always 0.
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	int i, j, k;
+	char *array;
+
+	k = 0;
+	array = malloc(sizeof(*s1 + *s2));
+	if (array == NULL)
+	{
+		return (NULL);
+	}
+	i = 0;
+	while (s1[i] != '\0')
+	{
+		array[k] = s1[i];
+		i++;
+		k++;
+	}
+	j = 0;
+	while (s2[j] != '\0')
+	{
+		array[k] = s2[j];
+		j++;
+		k++;
+	}
+	return (array);
+	free(array);
